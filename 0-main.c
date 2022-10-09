a613f53b234726d0ef6908e36c915c447ef2095f
lola10209 <shanteltestimony@gmail.com>
2022-10-09 11:33:10 -0700

    fix

--- 0-main.c
+++ 0-main.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAIKURE);
+		exit(EXIT_FAILURE);
 	}
 	monty_fd = fopen(argv[1], "r");
 
