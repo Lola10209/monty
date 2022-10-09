205df7f985f29ee8fe26d700ecd46a8ed5791864
lola10209 <shanteltestimony@gmail.com>
2022-10-09 13:10:37 -0700

    fix

--- 0-main.c
+++ 0-main.c
@@ -1,34 +1,30 @@
 #include "monty.h"
 
-global_t global = {EXIT_SUCCESS, NULL};
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+char **op_toks = NULL;
 
 /**
- * main - starting point monty interpreter
+ * main - the entry point for Monty Interp
  *
- * @argc: argument count
- * @argv: list of arguement passed to the function
+ * @argc: the count of arguments passed to the program
+ * @argv: pointer to an array of char pointers to arguments
  *
- * Return: EXIT_SUCCESS on success, EXIT_FAILURE otherwise
+ * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	FILE *monty_fd = NULL;
+	FILE *script_fd = NULL;
+	int exit_code = EXIT_SUCCESS;
 
 	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	monty_fd = fopen(argv[1], "r");
-
-	if (!monty_fd)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-
-	global.err_status = exec_monty(monty_fd);
-	fclose(monty_fd);
-
-	exit(global.err_status);
+		return (usage_error());
+	script_fd = fopen(argv[1], "r");
+	if (script_fd == NULL)
+		return (f_open_error(argv[1]));
+	exit_code = run_monty(script_fd);
+	fclose(script_fd);
+	return (exit_code);
 }
